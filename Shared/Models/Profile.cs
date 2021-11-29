9d7911d683895c514f0ebcf052c2956807ec5b7e
Hentus <Hentus@CODEINE-PC>
2021-11-29 15:57:54 +0200

    Add project files.

---
+++ Shared/Models/Profile.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorChatApp.Shared.Models
+{
+    public class Profile
+    {
+        public string _uName { get; set; }
+        public string _fName { get; set; }
+        public string _lName { get; set; }
+    }
+}
